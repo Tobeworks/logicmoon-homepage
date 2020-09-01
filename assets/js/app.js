49c2a090ee882f0a857c1f8e1c4fb7ef2b41a240
Tobias Lorsbach <hallo@tobeworks.de>
2020-09-01 18:39:27 +0200

    Nav buttons

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,13 +9,22 @@ $(document).ready(function(){
         $(".menu").slideToggle( "slow" );
     });
 
+    $('#hamburger-nav-button').click(function(){
+       $(this).fadeOut(500);
+       $('#overlay-menu').fadeIn(500);
+       ;
+        //gsap.fromTo("#overlay-menu", {opacity: 0}, {duration: 1.5, opacity: 1});
+    });
+
     $('#overlay-close-button').click(function(){
         $('#overlay-menu').fadeOut(500);
+        $('#hamburger-nav-button').fadeIn(500);
     });
-
-    $('#hamburger-nav-button').click(function(){
-        $('#overlay-menu').fadeIn(500);
-    });
-
-
+   
 }); 
+
+
+//init barba
+barba.init({
+    
+});
--- index.php
+++ index.php
@@ -2,31 +2,33 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-	<meta name="description" content="Homepage of Logic Moon Music">
-	<meta name="keywords" content="Logic Moon, Tobias Lorsbach, ambient, music">
+	<meta name="description" content="The Homepage of Logic Moon Music">
+	<meta name="keywords" content="Logic Moon, Tobias Lorsbach, ambient, Drone, Neoclassical, Texture, music">
 	<meta name="author" content="Tobias Lorsbach">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">	<link href="https://fonts.googleapis.com/css?family=Duru+Sans&display=swap" rel="stylesheet"> 
 	<title>Logic Moon Prepage</title>
-
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/animate.css">
 </head>
-<body>
-
+<body data-barba="wrapper">
 
 
 <div id="overlay-menu">
+
 	<div class="d-flex justify-content-end d-md-none"  id="overlay-close">
+	<div id="" class="button-nav-block">
 		<button class="btn btn-hamburger" id="overlay-close-button">
-			<svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-x-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+			<svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-x-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
 				<path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
 				<path fill-rule="evenodd" d="M11.854 4.146a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708-.708l7-7a.5.5 0 0 1 .708 0z"/>
 				<path fill-rule="evenodd" d="M4.146 4.146a.5.5 0 0 0 0 .708l7 7a.5.5 0 0 0 .708-.708l-7-7a.5.5 0 0 0-.708 0z"/>
 			</svg>
 		</button>
 	</div>
+	</div>
+	
 	<div class="overlay-middle">
 	<nav class="nav flex-column">
 				<a class="nav-link active" aria-current="page" href="#">Home</a>
@@ -39,7 +41,7 @@
 </div>
 
 <div class="d-flex justify-content-end d-md-none" id="hamburger-nav-sticky">
-	<div id="hamburger-nav-block">
+	<div id="" class="button-nav-block">
 	<button class="btn btn-hamburger" id="hamburger-nav-button">
 		<svg width="2em" height="2em" viewBox="0 0 16 16" fill="currentColor" class="bi bi-list text-light" xmlns="http://www.w3.org/2000/svg">
 		<path fill-rule="evenodd" d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
@@ -105,11 +107,11 @@
 				At vero eos et accusam et justo duo dolores et ea rebum. 
 				Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
 				<div>
-					<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/924081535&color=%23040303&auto_play=false&hide_related=true&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=true"></iframe>
+					<iframe height="300"  src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/924081535&color=%23040303&auto_play=false&hide_related=true&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=true"></iframe>
 				</div>
 				
 				<div>
-					<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/996748963&color=%23000000&auto_play=false&hide_related=true&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=true"></iframe>
+					<iframe height="300" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/996748963&color=%23000000&auto_play=false&hide_related=true&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=true"></iframe>
 				</div>
 				<div class="text-sm-center text-md-left"><button type="button" class="btn btn-lm">More Music</button></div>
 		</div>
@@ -256,13 +258,11 @@
 	
 </footer>
 
-<script
-			  src="https://code.jquery.com/jquery-3.5.1.min.js"
-			  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
-			  crossorigin="anonymous"></script>
-			  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.0/gsap.min.js_"></script>	
-	<script src="assets/js/app.js"></script>
-  </body>
+<script src="https://cdn.jsdelivr.net/npm/@barba/core"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.0/gsap.min.js"></script>	
+<script src="assets/js/app.js"></script>
+</body>
 </html>
